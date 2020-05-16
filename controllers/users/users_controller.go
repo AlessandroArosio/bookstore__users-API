64d897eac9f48759cf05e9ed9cda03a0adec6c45
Alessandro Arosio <alessandro.arosio1@gmail.com>
2020-05-16 20:53:50 +0100

    added DELETE

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -9,7 +9,7 @@ import (
 	"strconv"
 )
 
-func CreateUser(c *gin.Context) {
+func Create(c *gin.Context) {
 	var user users.User
 
 	if err := c.ShouldBindJSON(&user); err != nil {
@@ -26,11 +26,11 @@ func CreateUser(c *gin.Context) {
 	c.JSON(http.StatusCreated, result)
 }
 
-func GetUser(c *gin.Context) {
-	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
-	if userErr != nil {
+func Get(c *gin.Context) {
+	userId, idError := getUserId(c.Param("user_id"))
+	if idError != nil {
 		err := errors.NewBadRequestError("user id should be a number")
-		c.JSON(err.Status, err)
+		c.JSON(idError.Status, err)
 		return
 	}
 	user, getErr := services.GetUser(userId)
@@ -42,11 +42,11 @@ func GetUser(c *gin.Context) {
 	c.JSON(http.StatusOK, user)
 }
 
-func UpdateUser(c *gin.Context) {
-	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
-	if userErr != nil {
+func Update(c *gin.Context) {
+	userId, idError := getUserId(c.Param("user_id"))
+	if idError != nil {
 		err := errors.NewBadRequestError("user id should be a number")
-		c.JSON(err.Status, err)
+		c.JSON(idError.Status, err)
 		return
 	}
 
@@ -67,6 +67,29 @@ func UpdateUser(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, result)
+}
+
+func Delete(c *gin.Context) {
+	userId, idError := getUserId(c.Param("user_id"))
+	if idError != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(idError.Status, err)
+		return
+	}
+
+	if err := services.DeleteUser(userId); err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+	c.JSON(http.StatusOK, map[string]string{"status": "deleted"})
+}
+
+func getUserId(userIdParam string) (int64, *errors.RestErr) {
+	userId, userErr := strconv.ParseInt(userIdParam, 10, 64)
+	if userErr != nil {
+		return 0, errors.NewBadRequestError("user id should be a number")
+	}
+	return userId, nil
 }
 
 func SearchUser(c *gin.Context) {
