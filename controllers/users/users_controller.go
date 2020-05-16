9689c704cd41dd2c691df069b5d0e4c75a74da24
Alessandro Arosio <alessandro.arosio1@gmail.com>
2020-05-16 21:43:56 +0100

    adding search

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -84,14 +84,21 @@ func Delete(c *gin.Context) {
 	c.JSON(http.StatusOK, map[string]string{"status": "deleted"})
 }
 
+func Search(c *gin.Context) {
+	status := c.Query("status")
+
+	users, err := services.Search(status)
+	if err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+	c.JSON(http.StatusOK, users)
+}
+
 func getUserId(userIdParam string) (int64, *errors.RestErr) {
 	userId, userErr := strconv.ParseInt(userIdParam, 10, 64)
 	if userErr != nil {
 		return 0, errors.NewBadRequestError("user id should be a number")
 	}
 	return userId, nil
-}
-
-func SearchUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "todo")
 }
--- utils/date_utils/date_utils.go
+++ utils/date_utils/date_utils.go
@@ -4,6 +4,7 @@ import "time"
 
 const (
 	apiDateLayout = "2006-01-02T15:04:05Z" // just placeholders for time formatting
+	apiDbLayout   = "2006-01-02 15:01:04"
 )
 
 func GetNow() time.Time {
@@ -12,4 +13,8 @@ func GetNow() time.Time {
 
 func GetNowString() string {
 	return GetNow().Format(apiDateLayout)
+}
+
+func GetNowDbFormat() string {
+	return GetNow().Format(apiDbLayout)
 }
